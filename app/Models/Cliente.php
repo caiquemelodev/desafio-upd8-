5781b7fb68554380a564415d7ecb1f85e5402b16
caiquemelodev <caiquedev100@hotmail.com>
2025-07-14 19:38:56 -0300

    feat(model): adicionar model Cliente

---
+++ app/Models/Cliente.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cliente extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'nome', 'cpf', 'data_nascimento', 'sexo', 'endereco', 'cidade_id'
+    ];
+
+    public function cidade()
+    {
+        return $this->belongsTo(Cidade::class);
+    }
+}
