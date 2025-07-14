c5b21ef8a37246bf7bd5d1569d4efd4c10aa3344
caiquemelodev <caiquedev100@hotmail.com>
2025-07-14 19:38:19 -0300

    chore(routes): atualizar rotas da API

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\CidadeController;
+use App\Http\Controllers\RepresentanteController;
+
+Route::apiResource('clientes', ClienteController::class);
+Route::apiResource('cidades', CidadeController::class)->only(['index']);
+Route::apiResource('representantes', RepresentanteController::class);
